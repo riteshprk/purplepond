c4b86ed927b281562660d0ccb620d639a9d01986
riteshprk <reach2ritesh@gmail.com>
2020-07-01 14:09:01 +0100

    change media URL

--- purplepond/settings.py
+++ purplepond/settings.py
@@ -6,6 +6,8 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
+DEFAULT_FILE_STORAGE = os.getenv('CLOUDINARY_URL')
+
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
