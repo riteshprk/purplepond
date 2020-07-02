40242dbb6b1e635bde4bfbe22db933e65ee91846
riteshprk <reach2ritesh@gmail.com>
2020-07-02 14:19:55 +0100

    added multiple choices for size in form.py

--- core/views.py
+++ core/views.py
@@ -386,7 +386,7 @@ def add_to_cart(request, slug):
     order_item, created = OrderItem.objects.get_or_create(
         item=item,
         user=request.user,
-        size = ProductForm()
+        size = ProductForm(),
         ordered=False
     )
     order_qs = Order.objects.filter(user=request.user, ordered=False)
