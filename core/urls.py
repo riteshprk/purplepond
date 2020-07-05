f9bad6f5680d234ce50956edc6ecce4b1882faf6
riteshprk <reach2ritesh@gmail.com>
2020-07-05 14:37:16 +0100

    updated view.py

--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,10 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('order_summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('add_to_cart/<slug>/<size>', add_to_cart, name='add_to_cart'),
+    path('add_to_cart/<slug>', add_to_cart, name='add_to_cart'),
     path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
     path('remove_from_cart/<slug>', remove_from_cart, name='remove_from_cart'),
-    path('remove-item-from-cart/<slug>/<size>', remove_single_item_from_cart,
+    path('remove-item-from-cart/<slug>', remove_single_item_from_cart,
          name='remove-single-item-from-cart'),
     path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
     path('request-refund/', RequestRefundView.as_view(), name='request-refund')
