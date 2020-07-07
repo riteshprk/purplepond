9f0189402b9368c853dbb671b5ecb51ce38ddbe4
riteshprk <reach2ritesh@gmail.com>
2020-07-07 16:01:47 +0100

    updated cart_template_tags.py

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -12,6 +12,5 @@ def cart_item_count(request):
             count = 0
             for q in qs:
                 count += q.quantity
-            request.session['cart'] = cart_item_count
             return count
     return 0
