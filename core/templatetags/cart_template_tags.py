df3ce9b52cccbfee3f0bd95096a9fe1c45854f1a
riteshprk <reach2ritesh@gmail.com>
2020-07-07 12:42:34 +0100

    updated cart_template_tags.py

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -1,6 +1,6 @@
 
 from django import template
-from core.models import Order
+from core.models import Order, OrderItem
 
 register = template.Library()
 
@@ -10,5 +10,5 @@ def cart_item_count(user):
     if user.is_authenticated:
         qs = Order.objects.filter(user=user, ordered=False)
         if qs.exists():
-            return qs[0].items.count()
+            return qs[0].items.item.count()
     return 0
