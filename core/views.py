50cb94267ca92853c2644b5fa59732cbc55ca0db
riteshprk <reach2ritesh@gmail.com>
2020-07-14 15:25:10 +0100

    updated accound details

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
