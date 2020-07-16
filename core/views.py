70f6e9c8c8852f9ed98eb01771f7421d7ff71265
riteshprk <reach2ritesh@gmail.com>
2020-07-16 16:39:14 +0100

    updated accound details

--- core/views.py
+++ core/views.py
@@ -529,7 +529,7 @@ def remove_from_cart(request, slug, size):
     )[0]
     if order_item:
         order_item.delete()
-        order = Order.objects.get(user=request.user, items=None) or None
+        order = Order.objects.filter(user=request.user, items=None) or None
         if order:
             order.delete()
         messages.info(request, "This item was removed from your cart.")
@@ -579,7 +579,7 @@ def remove_single_item_from_cart(request, slug, size):
             return redirect("core:order-summary")
         else:
             order_item.delete()
-            order = Order.objects.get(user=request.user, items=None) or None
+            order = Order.objects.filter(user=request.user, items=None) or None
             if order:
                 order.delete()
             messages.info(request, "This item quantity was updated.")
